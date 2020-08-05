5ab180df570c6b2f31019dadc0e16e6c49a6768a
Ludvig Falck <lfalck@gmail.com>
2020-08-05 08:20:57 +0200

    Updated to Azure Functions V3. Simplified fetching from Key Vault by using Key Vault references instead of KeyVaultClient.

--- AzureFunctionsPGPEncrypt/PGPEncrypt.cs
+++ AzureFunctionsPGPEncrypt/PGPEncrypt.cs
@@ -1,26 +1,19 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using PgpCore;
-using System.Threading.Tasks;
-using Microsoft.Azure.KeyVault.Models;
-using System.Net.Http;
-using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.Azure.KeyVault;
-using System;
-using System.Text;
-using System.Collections.Concurrent;
-using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using PgpCore;
 
 namespace AzureFunctionsPGPEncrypt
 {
     public static class PGPEncrypt
     {
-        private static readonly HttpClient client = new HttpClient();
-        private static ConcurrentDictionary<string, string> secrets = new ConcurrentDictionary<string, string>();
+        private const string PublicKeyEnvironmentVariable = "pgp-public-key";
 
         [FunctionName(nameof(PGPEncrypt))]
         public static async Task<IActionResult> RunAsync(
@@ -29,55 +22,19 @@ namespace AzureFunctionsPGPEncrypt
         {
             log.LogInformation($"C# HTTP trigger function {nameof(PGPEncrypt)} processed a request.");
 
-            string publicKeyBase64 = req.Query["public-key"];
-            string publicKeyEnvironmentVariable = req.Query["public-key-environment-variable"];
-            string publicKeySecretId = req.Query["public-key-secret-id"];
+            string publicKeyBase64 = Environment.GetEnvironmentVariable(PublicKeyEnvironmentVariable);
 
-            if (publicKeyBase64 == null && publicKeyEnvironmentVariable == null && publicKeySecretId == null)
+            if (string.IsNullOrEmpty(publicKeyBase64))
             {
-                return new BadRequestObjectResult("Please pass a base64 encoded public key, an environment variable name, or a key vault secret identifier on the query string");
+                return new BadRequestObjectResult($"Please add a base64 encoded public key to an environment variable called {PublicKeyEnvironmentVariable}");
             }
 
-            if (publicKeyBase64 == null && publicKeyEnvironmentVariable != null)
-            {
-                publicKeyBase64 = Environment.GetEnvironmentVariable(publicKeyEnvironmentVariable);
-            }
-
-            if (publicKeyBase64 == null && publicKeySecretId != null)
-            {
-                try
-                {
-                    publicKeyBase64 = await GetPublicKeyAsync(publicKeySecretId);
-                }
-                catch (KeyVaultErrorException e) when (e.Body.Error.Code == "SecretNotFound")
-                {
-                    return new NotFoundResult();
-                }
-                catch (KeyVaultErrorException e) when (e.Body.Error.Code == "Forbidden")
-                {
-                    return new UnauthorizedResult();
-                }
-            }
-            byte[] data = Convert.FromBase64String(publicKeyBase64);
-            string publicKey = Encoding.UTF8.GetString(data);
-            req.EnableRewind(); //Make RequestBody Stream seekable
+            byte[] publicKeyBytes = Convert.FromBase64String(publicKeyBase64);
+            string publicKey = Encoding.UTF8.GetString(publicKeyBytes);
+            req.EnableBuffering(); //Make RequestBody Stream seekable
             Stream encryptedData = await EncryptAsync(req.Body, publicKey);
 
             return new OkObjectResult(encryptedData);
-        }
-
-        private static async Task<string> GetPublicKeyAsync(string secretIdentifier)
-        {
-            if (!secrets.ContainsKey(secretIdentifier))
-            {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                var authenticationCallback = new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback);
-                var kvClient = new KeyVaultClient(authenticationCallback, client);
-
-                SecretBundle secretBundle = await kvClient.GetSecretAsync(secretIdentifier);
-                secrets[secretIdentifier] = secretBundle.Value;
-            }
-            return secrets[secretIdentifier];
         }
 
         private static async Task<Stream> EncryptAsync(Stream inputStream, string publicKey)
